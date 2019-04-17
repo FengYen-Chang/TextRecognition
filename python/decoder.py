86c4e1a4938ed4d4e3b5c08391bae73a6951e419
FengYen-Chang <a60207john@gmail.com>
2019-04-17 13:40:50 +0800

    create decoder.py

---
+++ python/decoder.py
@@ -0,0 +1,11 @@
+###########################################
+# 
+#   Create and Implement  by John Feng
+#   
+#   Data :   
+#       2019/04/17:       
+#       Implement the CTC Greedy Decoder 
+#
+
+import numpy as np
+
